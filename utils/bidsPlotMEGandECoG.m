9a44e7ae98f8d58a1aa446417214873f9aaf55cd
Stephanie Montenegro <smontenegro26@gmail.com>
2019-08-30 11:29:47 -0400

    Updated help and comments

--- utils/bidsPlotMEGandECoG.m
+++ utils/bidsPlotMEGandECoG.m
@@ -1,29 +1,66 @@
-function bidsPlotMEGandECoG (projectDir, subject, session,runnums, tasks, ...
-    saveFolder,plotChecks, goodChannelRatio)%epocLength,
+function bidsPlotMEGandECoG (projectDir, subject, session, tasks,runnums, ...
+    goodChannelRatio,saveFolder,plotChecks)
 %
-% projectDir = '/Volumes/server/Projects/BAIR/Data/BIDS/visual';
-% subject = 'wlsubj100';
-% session = 'nyumeg01';
-% tasks = {'hrfpattern', 'temporalpattern', 'spatialobject', 'spatialpattern'};
-% saveFolder = 'MEGpreprocessed';
-% plotChecks = true;
-% runnums = [];
-% goodChannelRatio = .1;
-
+% bidsPlotMEGandECoG (projectDir, subject, session, [tasks],[runnums], ...
+%     [goodChannelRatio],[saveFolder],[plotChecks])
+%
+% Required input
+%     projectDir:         path where the BIDS projects lies (string)
+%     subject:            BIDS subject name (string, all lower case)
+%     session:            BIDS session name (string, all lower case)
+%
+%  Optional Input:
+%
+%     goodChannelRatio: decimal number indicating which proportion of the
+%                           MEG sensors within the area of interest to use
+%                           (cutoff by variance)
+%                            default: .25
+%     tasks:            one or more BIDS tasks (string or cell array of strings)
+%                           default: all tasks in session
+%     runnums:           BIDS run numbers (vector or cell array of vectors)
+%                           default: all runs for specified tasks
+%     saveFolder:       Name of folder to save images (string)
+%                           default: none (does not save images)
+%     plotChecks:       whether to plot sanity checks (boolean)
+%
+% Example:
+%     projectDir       = '/Volumes/server/Projects/BAIR/Data/BIDS/visual';
+%     subject          = 'wlsubj100';
+%     session          = 'nyumeg01';
+%     tasks            = {'hrfpattern', 'temporalpattern', 'spatialobject', 'spatialpattern'};
+%     runnums          = [];
+%     saveFolder       = 'MEGpreprocessed';
+%     plotChecks       = true;
+%     goodChannelRatio = .1;
+%
+%     bidsPlotMEGandECoG (projectDir, subject, session, tasks,runnums, saveFolder,plotChecks, goodChannelRatio)
+%
 %% Set up paths and options
+if ~exist('projectDir', 'var') || isempty(projectDir)
+    error('projectDir not defined');
+end
+if ~exist('subject', 'var') || isempty(subject)
+    error('subject not defined');
+end
+if ~exist('session', 'var') || isempty(session)
+    error('session not defined');
+end
+if ~exist('tasks', 'var'), tasks = []; end
+if ~exist('runnums', 'var'), runnums = []; end
+
 [session, tasks, runnums,siteModality ] = bidsSpecifyEPIs(projectDir, subject, session, tasks, runnums);
 
 switch siteModality
     case 'meg'
-        dataChannels            = 1:157;
-        %environmentalChannels   = 158:160;
-        %triggerChannels         = 161:168;
         rawDataFldr = 'meg';
-        dataStr = 'meg';
-        unit = 'Magnetic flux (T)';
+        dataStr     = 'meg';
+        unit        = 'Magnetic flux (T)';
+        if ~exist('goodChannelRatio', 'var') || isempty(goodChannelRatio)
+            goodChannelRatio = .25;
+        end
     case 'ecog'
         rawDataFldr = 'ieeg';
-        dataStr = 'ieeg';
+        dataStr     = 'ieeg';
 end
 
 rawDataDir  = fullfile (projectDir, sprintf('sub-%s', subject), sprintf('ses-%s', session),rawDataFldr);
@@ -32,7 +69,7 @@ if ~exist('saveFolder', 'var')|| isempty(saveFolder)
     saveData = false;
 else
     outputDir   = fullfile (projectDir,'derivatives', saveFolder, sprintf('sub-%s', subject), sprintf('ses-%s', session));
-    saveData = true;
+    saveData    = true;
     if ~exist('outputDir', 'dir'), mkdir(outputDir); end
 end
 
@@ -43,51 +80,61 @@ for ii = 1:length (tasks)
     % initialize some variables
     allOnsets   = []; allTriggers = [];fullTimeseries = []; allStimNames = []; allStimNums = [];
     
+    % concatenate all runs for this task
     for jj = 1:length(runnums{ii})
-        thisTimeSeries = ts{jj};
-        expLength      = length(thisTimeSeries);
+        % Get TSV information for each run
+        thisTimeSeries          = ts{jj};
+        expLength               = length(thisTimeSeries);
         [onsets,stimTypes , trigNums, stimTrigger] = parseTSVfile(rawDataDir, tasks{ii}, runnums{ii}(jj), expLength);
+  
         if jj == 1, allOnsets   = [allOnsets; onsets];end
         if jj > 1,  allOnsets   = [allOnsets; (onsets + allOnsets(end))];end
-        allTriggers = [allTriggers; stimTrigger];
-        allStimNames = [allStimNames; string(stimTypes)];
-        fullTimeseries = cat(2,fullTimeseries,thisTimeSeries);
-        allStimNums = [allStimNums; trigNums];
+        allTriggers             = [allTriggers; stimTrigger];
+        allStimNames            = [allStimNames; string(stimTypes)];
+        fullTimeseries          = cat(2,fullTimeseries,thisTimeSeries);
+        allStimNums             = [allStimNums; trigNums];
     end
     stimNums = unique(allStimNums);
     
     switch siteModality
         case 'meg'
             % Define length of epochs relative to stimTrigger onset and get sample rate
+            dataChannels            = 1:157;
+            %environmentalChannels   = 158:160;
+            %triggerChannels         = 161:168;
             epochLength = [0 1]; % start and end of epoch (seconds), zero is stimTrigger onset
-            fs      = 1000;  % Hz
-            % Get sensordata (time x epochs x channels) and conditions (epochs x 1)
+            fs          = 1000;  % Hz
+            % Get sensordata (time x epochs x channels)
             [epocedData, ~] = meg_make_epochs(fullTimeseries', allOnsets, epochLength, fs);
-            sensorsToPlot = findMEGChannels(hdr{1}, tasks{ii},plotChecks);
+            
+            % figure out which sensors to plot and pick a subset with the
+            % highest variance to plot later
+            sensorsToPlot    = findMEGChannels(hdr{1}, tasks{ii},plotChecks);
             sensorNumsToPlot = dataChannels(sensorsToPlot);
-            goodChannels = findGoodMEGChannels(sensorNumsToPlot, epocedData, goodChannelRatio,plotChecks);
+            goodChannels     = findGoodMEGChannels(sensorNumsToPlot, epocedData, goodChannelRatio,plotChecks);
             
             for ss = 1:length(stimNums)
                 idx = allStimNums == stimNums(ss);
                 % mean timeseries
-                mnTS{ss} = squeeze(mean(epocedData(:,idx,:),2));
+                mnTS{ss}      = squeeze(mean(epocedData(:,idx,:),2));
                 % Transform to Fourier space and take the amplitudes
-                amps = abs(fft(epocedData(:,idx,:)))/length(epocedData(1))*2;
+                amps          = abs(fft(epocedData(:,idx,:)))/length(epocedData(1))*2;
                 % Take mean across epochs, rescale by number of timepoints
-                meanAmps{ss} = squeeze(mean(amps,2));
-                tmp = find(idx);
+                meanAmps{ss}  = squeeze(mean(amps,2));
+                tmp           = find(idx);
                 stimNames{ss} = allStimNames(tmp(1));
             end
         case 'ecog'
             % do something
             
     end
-    t = 0:(size(epocedData,1)-1);
-    f = 0:length(t)-1;
-    
+    % plots triggers and a an average timeseries across epocs
     if plotChecks
         plotSanityChecks (epocedData, allTriggers, unit)
     end
+    
+    % for the sake of making plots easier to see, split experiment
+    % conditions in half
     if length(stimNums) > 3
         stimNumsToPlot{1} = stimNums(1:length(stimNums)/2);
         stimNamesToPlot{1} = stimNames(1:length(stimNames)/2);
@@ -98,7 +145,11 @@ for ii = 1:length (tasks)
         stimNamesToPlot{1} = stimNames;
     end
     
-    % Plot mean time series and amplitudes averaged across sensors in one plot
+    %% Plot it
+    
+    t = 0:(size(epocedData,1)-1);
+    f = 0:length(t)-1;
+    % Plot mean time series and amplitudes averaged across "good" sensors in one plot
     for c = 1:length(stimNumsToPlot)
         thisfig = figure('Name',sprintf('%s - set%d',tasks{ii}, c)); clf;
         subplot(1,2,1); cla;
@@ -106,12 +157,11 @@ for ii = 1:length (tasks)
             tmpIdx = stimNums == stimNumsToPlot{c}(n);
             plot(t, mean(mnTS{tmpIdx}(:,goodChannels),2)), hold on;
         end
-        %xlim([min(t) max(t)]);
         xlabel('Time (ms)')
         ylabel(unit)
         title(sprintf('MeanTS (Across %d Sensors)', length(goodChannels)))
         set(gca, 'TickDir', 'out', 'FontSize', 12); box off;
-       legend (stimNamesToPlot{c},'Location','best')
+        legend (stimNamesToPlot{c},'Location','best')
         box off;
         
         subplot(1,2,2); cla;
@@ -126,95 +176,91 @@ for ii = 1:length (tasks)
         set(gca, 'TickDir', 'out', 'FontSize', 12); box off;
         set(thisfig, 'Position', (thisfig.Position.*[1 1 2 2]))
         legend (stimNamesToPlot{c},'Location','best')
-        %save
+        
         if saveData
             print (fullfile(outputDir,sprintf('sub-%s_%s_AvgAcross%dSensors_1',...
                 subject,tasks{ii}, length(goodChannels))),'-dpng')
         end
     end
-
-% Plot the mean time series and amplitudes for every sensor on the
-% same plots with subplots by condition
-thisfig = figure('Name',subject) ; clf;
-figCount = 1;
-for s = 1:length(stimNums)
-    subplot(length(stimNums),2,figCount); cla;
-    plot(t, mnTS{s}(:,goodChannels));
-    title(sprintf('%s MeanTS - Top %2.1f %% of Channels',stimNames{s},(length(goodChannels)/157)*100))
-    ylabel(unit)
-    xlabel([])
-    set(gca, 'TickDir', 'out', 'FontSize', 12); box off;
-    box off;
     
-    if  figCount == length(stimNums), xlabel('Time (ms)'), end
-    figCount = figCount+1;
-    
-    subplot(length(stimNums),2,figCount); cla;
-    plot(f, meanAmps{s}(:,goodChannels));
-    xlabel([])
-    ylabel('Amp(T)')
-    title(sprintf('%s MeanFFT - Top %2.1f %% of Channels',stimNames{s},(length(goodChannels)/157)*100))
-    set(gca, 'XScale', 'log', 'YScale', 'log')
-    set(gca, 'TickDir', 'out', 'FontSize', 12); box off;
-    
-    if figCount == length(stimNums)*2,xlabel('Frequency (Hz)'),end
-    figCount = figCount+1;
-end
-% if length(stimNums) >1
-%     linkaxes(thisfig.Children(1:2:end),'xy')
-%     linkaxes(thisfig.Children(2:2:end),'xy')
-% end
-legend(string(goodChannels),'Orientation','horizontal')
-set(thisfig, 'Position', (thisfig.Position.*[1 1 2 2]))
-%save
-if saveData
-    print (fullfile(outputDir,sprintf('sub-%s_%s_MEG_multiSensor',...
-        subject,tasks{ii})),'-dpng')
-end
-
-%Plot subplots for each sensor containing multiple stimuli
-for c = 1:length(stimNumsToPlot)
-    for j = 1:2:length(goodChannels)
-        thisfig = figure; clf;
-        subplot(1,2,1); cla;
-        for n = 1:length (stimNumsToPlot{c})
-            tmpIdx = stimNums == stimNumsToPlot{c}(n);
-            plot(t, mnTS{tmpIdx}(:,goodChannels(j)));hold on
-        end
-        legend (stimNamesToPlot{c},'Location','best')
-        xlabel('Time (ms)')
+    % Plot the mean time series and amplitudes for every sensor on the
+    % same plots with subplots by condition
+    thisfig = figure('Name',subject) ; clf;
+    figCount = 1;
+    for s = 1:length(stimNums)
+        subplot(length(stimNums),2,figCount); cla;
+        plot(t, mnTS{s}(:,goodChannels));
+        title(sprintf('%s MeanTS - Top %2.1f %% of Channels',stimNames{s},(length(goodChannels)/157)*100))
         ylabel(unit)
-        title(sprintf('MnTS - %s',string(goodChannels(j))))
+        xlabel([])
         set(gca, 'TickDir', 'out', 'FontSize', 12); box off;
         box off;
-       
-        subplot(1,2,2); cla;
-        for n = 1:length (stimNumsToPlot{c})
-            tmpIdx = stimNums == stimNumsToPlot{c}(n);
-            plot(f, meanAmps{tmpIdx}(:,goodChannels(j)));hold on
-        end
-        xlabel('Frequency (Hz)')
-        ylabel('Amplitudes (T)')
-        title(sprintf('MnFFT - %s',string(goodChannels(j))))
-        legend (stimNamesToPlot{c},'Location','best')
+        
+        if  figCount == length(stimNums), xlabel('Time (ms)'), end
+        figCount = figCount+1;
+        
+        subplot(length(stimNums),2,figCount); cla;
+        plot(f, meanAmps{s}(:,goodChannels));
+        xlabel([])
+        ylabel('Amp(T)')
+        title(sprintf('%s MeanFFT - Top %2.1f %% of Channels',stimNames{s},(length(goodChannels)/157)*100))
         set(gca, 'XScale', 'log', 'YScale', 'log')
         set(gca, 'TickDir', 'out', 'FontSize', 12); box off;
-        set(thisfig, 'Position', (thisfig.Position.*[1 1 2 2]))
-    %save
+        
+        if figCount == length(stimNums)*2,xlabel('Frequency (Hz)'),end
+        figCount = figCount+1;
+    end
+    % if length(stimNums) >1
+    %     linkaxes(thisfig.Children(1:2:end),'xy')
+    %     linkaxes(thisfig.Children(2:2:end),'xy')
+    % end
+    %legend(string(goodChannels),'Orientation','horizontal')
+    set(thisfig, 'Position', (thisfig.Position.*[1 1 2 2]))
+    
     if saveData
-        print (fullfile(outputDir,sprintf('sub-%s_%s_MEG_sensor%d_%d',...
-            subject,tasks{ii}, goodChannels(j), c)),'-dpng')
+        print (fullfile(outputDir,sprintf('sub-%s_%s_MEG_multiSensor',...
+            subject,tasks{ii})),'-dpng')
     end
+
+    %Plot subplots for each sensor containing multiple stimuli
+    for c = 1:length(stimNumsToPlot)
+        for j = 1:2:length(goodChannels)
+            thisfig = figure; clf;
+            subplot(1,2,1); cla;
+            for n = 1:length (stimNumsToPlot{c})
+                tmpIdx = stimNums == stimNumsToPlot{c}(n);
+                plot(t, mnTS{tmpIdx}(:,goodChannels(j)));hold on
+            end
+            legend (stimNamesToPlot{c},'Location','best')
+            xlabel('Time (ms)')
+            ylabel(unit)
+            title(sprintf('MnTS - %s',string(goodChannels(j))))
+            set(gca, 'TickDir', 'out', 'FontSize', 12); box off;
+            box off;
+            
+            subplot(1,2,2); cla;
+            for n = 1:length (stimNumsToPlot{c})
+                tmpIdx = stimNums == stimNumsToPlot{c}(n);
+                plot(f, meanAmps{tmpIdx}(:,goodChannels(j)));hold on
+            end
+            xlabel('Frequency (Hz)')
+            ylabel('Amplitudes (T)')
+            title(sprintf('MnFFT - %s',string(goodChannels(j))))
+            legend (stimNamesToPlot{c},'Location','best')
+            set(gca, 'XScale', 'log', 'YScale', 'log')
+            set(gca, 'TickDir', 'out', 'FontSize', 12); box off;
+            set(thisfig, 'Position', (thisfig.Position.*[1 1 2 2]))
+            
+            if saveData
+                print (fullfile(outputDir,sprintf('sub-%s_%s_MEG_sensor%d_%d',...
+                    subject,tasks{ii}, goodChannels(j), c)),'-dpng')
+            end
+        end
+        
     end
-   
-end
-clear stimNamesToPlot stimNumsToPlot stimNames stimNums
+    clear stimNamesToPlot stimNumsToPlot stimNames stimNums
 end
 end
-
-
-
-
 
 function [onsets,stimTypes , trigNums, stimTrigger] = parseTSVfile (rawDataDir, task, runnum, expLength)
 %load in the TSV file information to confirm everything lines up
@@ -227,7 +273,7 @@ stimTrigger = zeros( expLength,1);
 stimTrigger(round(onsets*1000)) = trigNums;
 end
 
-function sensorsToPlot = findMEGChannels(hdr, task,plotChecks)  %fullTimeseries,dataChannels, epocedData, goodChannelRatio)
+function sensorsToPlot = findMEGChannels(hdr, task,plotChecks)  
 layout = ft_prepare_layout([],hdr);
 % Define x and y sensor positions
 xpos = layout.pos(1:157,1);
@@ -235,14 +281,15 @@ ypos = layout.pos(1:157,2);
 
 switch task
     case {'temporalpattern', 'spatialpattern', 'spatialobject', 'hrf','hrfpattern', 'prf'}
+        % Get sensors over the lower back of the head (presumably occipital cortex)
         whichSensors = find(ypos<0 & xpos<1);
     case {'boldhand', 'fingermappingright','fingermappingleft', 'gestures', 'boldsat1', 'boldsat2' , 'boldsat3' , 'boldsat4' }
-        
+        % figure this out
 end
 sensorsToPlot = zeros(1,157);
 sensorsToPlot(whichSensors) = 1;
 sensorsToPlot = boolean(sensorsToPlot);
-if plotChecks% Get sensors over the lower back of the head (presumably occipital cortex)
+if plotChecks
     % Plot the selected sensors
     clims = [0 1];
     cmap  = colormap('gray');
