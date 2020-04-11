3e45b846f895472925bb6e6c6a7fbf995f348aed
irisgroen <iris.groen@nyu.edu>
2020-04-11 13:59:10 -0400

    now including glasser and rosenke in command in addition to wang and benson

--- scripts/run_docker_retinotopyatlas.sh
+++ scripts/run_docker_retinotopyatlas.sh
@@ -1,5 +1,4 @@
-subjectID=som726
+subjectID=som723
 bidsFolder=/Volumes/server/Projects/BAIR/Data/BIDS/visual 
 
-docker run --rm -it -v ${bidsFolder}/derivatives/freesurfer:/subjects/ nben/neuropythy atlas sub-${subjectID} --verbose\
-#docker run --rm -it -v ${bidsFolder}/derivatives/freesurfer:/subjects/ nben/neuropythy atlas sub-${subjectID} --atlases=glasser16,benson14,wang15,rosenke18 --verbose\
+docker run --rm -it -v ${bidsFolder}/derivatives/freesurfer:/subjects/ nben/neuropythy atlas sub-${subjectID} --atlases=glasser16,benson14,wang15,rosenke18 --verbose\
