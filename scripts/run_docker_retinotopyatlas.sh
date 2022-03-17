5f3266899978a2a3b96e1578a6a8520d6a72c7ea
Iris Groen <iris.groen@nyu.edu>
2022-03-17 16:43:13 +0100

    separated commands for different atlases and added note on memory issues

--- scripts/run_docker_retinotopyatlas.sh
+++ scripts/run_docker_retinotopyatlas.sh
@@ -1,4 +1,12 @@
-subjectID=som723
-bidsFolder=/Volumes/server/Projects/BAIR/Data/BIDS/visual 
+docker pull nben/neuropythy
+# note: if Docker doesn't run all requested atlases, change memory settings on Docker to use more RAM (see https://github.com/noahbenson/neuropythy/issues/27) 
+subjectID=p03
+bidsFolder=/Users/iiagroen/surfdrive/BAIR/BIDS/visual_ecog_recoded
 
-docker run --rm -it -v ${bidsFolder}/derivatives/freesurfer:/subjects/ nben/neuropythy atlas sub-${subjectID} --atlases=glasser16,benson14,wang15,rosenke18 --verbose\
+docker run --rm -it -v ${bidsFolder}/derivatives/freesurfer:/subjects/ nben/neuropythy atlas sub-${subjectID} --atlases=benson14 --verbose\
+
+docker run --rm -it -v ${bidsFolder}/derivatives/freesurfer:/subjects/ nben/neuropythy atlas sub-${subjectID} --atlases=wang15 --verbose\
+
+docker run --rm -it -v ${bidsFolder}/derivatives/freesurfer:/subjects/ nben/neuropythy atlas sub-${subjectID} --atlases=glasser16 --verbose\
+
+docker run --rm -it -v ${bidsFolder}/derivatives/freesurfer:/subjects/ nben/neuropythy atlas sub-${subjectID} --atlases=rosenke18 --verbose\
